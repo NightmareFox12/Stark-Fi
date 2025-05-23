14d96821be89f648a43bb4176d6d61a4631cab41
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-05-22 23:55:52 -0400

    Update dependencies and implement fl_chart in HomeScreen
    - Added fl_chart package to pubspec.yaml for charting capabilities.
    - Updated pubspec.lock to include equatable and fl_chart dependencies.
    - Imported fl_chart in HomeScreen and modified the body layout for future chart integration.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:stark_fi/theme/stark_fi_theme.dart';
@@ -13,7 +14,40 @@ class HomeScreen extends HookWidget {
           title: Text("Home"),
           backgroundColor: StarkFiTheme(context).primaryColor,
         ),
-        body: Column(children: [Text("Ganains")]),
+        body: Column(
+          children: [
+            Text("akakak")
+            // BarChart(
+            //   BarChartData(
+            //     rotationQuarterTurns: 4,
+            //     barGroups: [
+            //       BarChartGroupData(
+            //         x: 1,
+            //         barsSpace: 10,
+            //         groupVertically: true,
+            //         barRods: [BarChartRodData(toY: 2)],
+            //       ),
+
+            //       BarChartGroupData(
+            //         x: 2,
+            //         barsSpace: 10,
+            //         groupVertically: true,
+            //         barRods: [BarChartRodData(toY: 2)],
+            //       ),
+
+            //       BarChartGroupData(
+            //         x: 3,
+            //         barsSpace: 10,
+            //         groupVertically: true,
+            //         barRods: [BarChartRodData(toY: 2)],
+            //       ),
+            //     ],
+            //   ),
+            //   duration: Duration(milliseconds: 150), // Optional
+            //   curve: Curves.linear, // Optional
+            // ),
+          ],
+        ),
         bottomNavigationBar: BottomNavigationBar(
           items: [
             BottomNavigationBarItem(
