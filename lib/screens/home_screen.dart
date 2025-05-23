d8a9ea02abfc5643ac99e7af81084a965ea307d2
nightmarefox12 <carloshenriquez392004@gmail.com>
2025-05-23 09:49:11 -0400

    Update dependencies, implement reown_appkit, and modify AndroidManifest
    - Added reown_appkit dependency to pubspec.yaml for enhanced wallet functionality.
    - Updated pubspec.lock to include new dependencies and their versions.
    - Modified AndroidManifest files to include internet permission and package queries for wallet integration.
    - Introduced ModalTest widget in HomeScreen for testing reown_appkit modal functionality.
    - Registered url_launcher plugin for Linux and Windows platforms.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:stark_fi/theme/stark_fi_theme.dart';
+import 'package:stark_fi/wallet/modal_test.dart';
 
 class HomeScreen extends HookWidget {
   const HomeScreen({super.key});
@@ -16,7 +17,8 @@ class HomeScreen extends HookWidget {
         ),
         body: Column(
           children: [
-            Text("akakak")
+            Text("akakak"),
+            ModalTest(),
             // BarChart(
             //   BarChartData(
             //     rotationQuarterTurns: 4,
